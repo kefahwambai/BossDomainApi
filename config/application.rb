06c76fad4c3d4594e334b5c3eaca10a7bb43960a
KefahWambai <kefah.ngwei@student.moringaschool.com>
2023-12-05 18:13:58 +0300

    jenkins config

--- config/application.rb
+++ config/application.rb
@@ -13,15 +13,11 @@ require "action_text/engine"
 require "action_view/railtie"
 require "action_cable/engine"
 
-# require "rails/test_unit/railtie"
-
-# Require the gems listed in Gemfile, including any gems
-# you've limited to :test, :development, or :production.
 Bundler.require(*Rails.groups)
 
 module Bossapi
   class Application < Rails::Application
-    # Initialize configuration defaults for originally generated Rails version.
+ 
     config.load_defaults 7.0
     Dotenv.load(*Dir[Rails.root.join(".env*")])    
     config.api_only = true
